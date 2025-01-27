89d10cfe7cb551e2d729efb0d8da9131e0eb7365
Sofie Kedvik <sofie.kedvik@gmail.com>
2025-01-27 12:08:19 +0100

    feat: selected filters

--- src/ui/components/product-grid/product-grid.tsx
+++ src/ui/components/product-grid/product-grid.tsx
@@ -66,48 +66,52 @@ export default function ProductGrid({
   return (
     <div>
       <div className="bg-white">
-        <div className="flex gap-6 items-center py-6">
-          <h3 className="text-md tracking-tight text-gray-900">
-            <span className="font-bold">Products:</span>{" "}
-            <span>{productsGrid.length || 0} </span>
-          </h3>
-          <form>
-            <select
-              name="category"
-              id="category"
-              className="border border-gray-300 rounded-md p-2"
-              onChange={handleChangeCategory}
-              value={selectedCategory}
-            >
-              <option value="">All</option>
-              {categories.map((category) => (
-                <option key={category} value={category}>
-                  {category}
-                </option>
-              ))}
-            </select>
-          </form>
-          {selectedCategory && (
+        <div className="flex gap-6 justify-between items-center py-6">
+          <div className="flex gap-6 items-center">
+            <h3 className="text-md tracking-tight text-gray-900">
+              <span className="font-bold">Products:</span>{" "}
+              <span>{productsGrid.length || 0} </span>
+            </h3>
             <div className="flex gap-3 items-center">
               <h3 className="text-md tracking-tight text-gray-900">
                 <span className="font-bold">Category:</span>{" "}
               </h3>
+              <form>
+                <select
+                  name="category"
+                  id="category"
+                  className="border border-gray-300 rounded-md p-2"
+                  onChange={handleChangeCategory}
+                  value={selectedCategory}
+                >
+                  <option value="">All</option>
+                  {categories.map((category) => (
+                    <option key={category} value={category}>
+                      {category}
+                    </option>
+                  ))}
+                </select>
+              </form>
+            </div>
+          </div>
+          <div>
+            <h3 className="text-md tracking-tight text-gray-900">
+              <span className="font-bold">Filters:</span>{" "}
+            </h3>
+            {!selectedCategory && <span className="text-sm">None</span>}
+            {selectedCategory && (
               <button
-                className="flex bg-slate-200 rounded-md p-2"
+                className="flex bg-slate-200 rounded-md p-1 text-sm"
                 type="button"
                 onClick={unSelectCategory}
               >
                 {selectedCategory}{" "}
-                <span
-                  type="button"
-                  onClick={unSelectCategory}
-                  className="w-6 h-6 flex items-center justify-center pl-2"
-                >
+                <span className="w-6 h-6 flex items-center justify-center pl-2 text-sm">
                   <XCircleIcon title="remove" />
                 </span>
               </button>
-            </div>
-          )}
+            )}
+          </div>
         </div>
         {productsGrid.length ? (
           <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
